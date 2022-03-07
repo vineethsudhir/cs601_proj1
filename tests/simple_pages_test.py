66394da608e0d0ba77fe9955bc10b4973dbbfa67
vineethsudhir <vineeth.sudhir@gmail.com>
2022-03-07 17:50:18 -0500

    Updated tests

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -14,37 +14,32 @@ def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Index Page" in response.data
+    assert b"Home" in response.data
 
 def test_request_about(client):
     """This makes the index page"""
     response = client.get("/git")
     assert response.status_code == 200
-    assert b"About Page" in response.data
+    assert b"Git" in response.data
 
 def test_request_page1(client):
     """This makes the index page"""
     response = client.get("/docker")
     assert response.status_code == 200
-    assert b"Page 1" in response.data
+    assert b"Docker" in response.data
 
 def test_request_page2(client):
     """This makes the index page"""
     response = client.get("/pyflask")
     assert response.status_code == 200
-    assert b"Page 2" in response.data
+    assert b"Python/Flask" in response.data
 
 def test_request_page3(client):
     """This makes the index page"""
     response = client.get("/cid")
     assert response.status_code == 200
-    assert b"Page 3" in response.data
+    assert b"Continuous Integration and Deployment" in response.data
 
-def test_request_page4(client):
-    """This makes the index page"""
-    response = client.get("/page4")
-    assert response.status_code == 200
-    assert b"Page 4" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""
